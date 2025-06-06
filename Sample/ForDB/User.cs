9e4e71a445846e3e8a9b760e8229e07c962ba766
MrMoustacheOwO <besman80ber@gmail.com>
2025-06-07 00:13:47 +0400

    Добавлено: СУБД, логика отправки данных в БД при регистрации, окно для авторизации(регистрации)

---
+++ Sample/ForDB/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.ForDB
+{
+    internal class User
+    {
+        public int ID {  get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public User(string login, string email, string pass)
+        {
+            this.Login = login;
+            this.Email = email; 
+            this.Password = pass;
+        }
+    }
+}
---
+++ Sample/ViewModels/AutorisationWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Sample.ForDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.ViewModels
+{
+    internal class AutorisationWindowViewModel
+    {
+       private ApplicationContext db;
+
+        
+        public AutorisationWindowViewModel()
+        {
+            db = new ApplicationContext();
+        }
+
+
+        /*Тут типо должна быть логика авторизации и регистрации, но ты пока ее не сделала(
+         * Региастрация идет форма для заполнения, логин имайл и пароль
+         * Там типо проверки на то то то
+         * При нажатии на кнопку ты запускаешь проверку на корректные данные и в коце вызываешь это
+        */
+        //User user=new User(login,email,password);
+        //db.Users.Add(user);
+        //db.SaveChanges();
+    }
+}
